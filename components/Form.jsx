939f052a1436f58fd1cd7a3192b6a223746ec973
devSoni <soni.hiren108@gmail.com>
2023-05-23 13:30:26 +0200

    Form update

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,46 +1,9 @@
-import { useState } from 'react';
-
 const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [message, setMessage] = useState('');
-
-  async function handleOnSubmit(e) {
-    e.preventDefault();
-
-    const formData = {
-      name,
-      email,
-      phoneNumber,
-      message,
-    };
-
-    try {
-      const response = await fetch('/api/mail', {
-        method: 'post',
-        body: JSON.stringify(formData),
-      });
-
-      if (!response.ok) {
-        throw new Error('Form submission failed');
-      }
-
-      // Reset form fields
-      setName('');
-      setEmail('');
-      setPhoneNumber('');
-      setMessage('');
-    } catch (error) {
-      // Handle error
-      console.error(error);
-    }
-  }
 
   return (
     <div className="bg-stone-950 px-4 sm:px-56 pb-16">
       <div className="rounded-lg isolate bg-white p-8">
-        <form name='contact' method="post" data-netlify='true' className="mx-auto max-w-xl" onSubmit={handleOnSubmit}>
+        <form name='contact' method="post" data-netlify='true' className="mx-auto max-w-xl">
           <div className="sm:col-span-2">
             <div>
               <label htmlFor="name" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -53,8 +16,6 @@ const ContactForm = () => {
                   id="name"
                   autoComplete="full-name"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-500 sm:text-sm sm:leading-6"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
             </div>
@@ -69,8 +30,6 @@ const ContactForm = () => {
                   id="email"
                   autoComplete="email"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-500 sm:text-sm sm:leading-6"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
             </div>
@@ -85,8 +44,6 @@ const ContactForm = () => {
                   id="phone-number"
                   autoComplete="tel"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-500 sm:text-sm sm:leading-6"
-                  value={phoneNumber}
-                  onChange={(e) => setPhoneNumber(e.target.value)}
                 />
               </div>
             </div>
@@ -100,8 +57,6 @@ const ContactForm = () => {
                   id="message"
                   rows={4}
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-500 sm:text-sm sm:leading-6"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
                 />
               </div>
             </div>
