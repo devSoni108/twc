10690f56dc34ecaca88bfc85d00dd7243e478af5
devSoni <soni.hiren108@gmail.com>
2023-06-26 17:49:24 +0200

    Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
-    let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
-
     return (
       <Html lang="en">
         <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GTM_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-            }}
-          />
-
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KLL47LN');`,
+              }}
+        />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
@@ -31,6 +22,12 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <noscript
+    dangerouslySetInnerHTML={{
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KLL47LN"
+      height="0" width="0" style="display:none;visibility:hidden"/>`,
+    }}
+  />
         </body>
       </Html>
     );
